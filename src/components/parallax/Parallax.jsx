9ae10f2f765c5d443ebcf2b65afccae8fcf8befd
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-01-15 10:21:15 -0300

    Atualização de Funcionalidade _2
    Finalizado as páginas com efeitos Parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -11,16 +11,33 @@ const Parallax = ({type}) => {
         offset:["start start", "end start"]
     })
 
-    const yBg=useTransform(scrollYProgress, [0,1], ["-50%", "300%"])
+    const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
 
     return (
-    <div className="parallax">
-        <motion.h1 style={{y:yBg}}>{type==="services" ? "What We Do?" : "What We Did"}</motion.h1>
+    <div className="parallax" 
+    ref={ref} 
+    style={{
+        background: 
+            type === "services"
+                ? "linear-gradient(180deg, #111132, #0c0c1d)"
+                : "linear-gradient(180deg, #111132, #505064)",
+            }}>
+                
+        <motion.h1 style={{ y: yText }}>{type==="services" ? "What We Do?" : "What We Did"}</motion.h1>
         <motion.div className="mountains"></motion.div>
-        <motion.div className="planets"></motion.div>
-        <motion.div className="stars"></motion.div>
+        <motion.div 
+            className="planets" 
+            style={{
+                y: yBg, 
+                backgroundImage: `url(${type === "services" ? "/planets.png" : "sun.png"})`}}>
+        </motion.div>
+        <motion.div style={{ x: yBg }} className="stars"></motion.div>
     </div>
   )
 }
+
+
+
 
 export default Parallax
