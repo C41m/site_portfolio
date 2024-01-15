750e03df1153db8f45b676239d1e1f4dd806fc3b
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-01-15 09:01:43 -0300

    Novas Funcionalidades ao Site _1

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,26 @@
+import {useRef} from "react"
+import "./parallax.scss"
+import {motion, useScroll, useTransform} from "framer-motion"
+
+const Parallax = ({type}) => {
+
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target:ref,
+        offset:["start start", "end start"]
+    })
+
+    const yBg=useTransform(scrollYProgress, [0,1], ["-50%", "300%"])
+
+    return (
+    <div className="parallax">
+        <motion.h1 style={{y:yBg}}>{type==="services" ? "What We Do?" : "What We Did"}</motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div className="planets"></motion.div>
+        <motion.div className="stars"></motion.div>
+    </div>
+  )
+}
+
+export default Parallax
