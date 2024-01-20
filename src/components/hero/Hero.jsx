66d11b6b09b962872ffb48fe3fb655bba1e9ac94
Caio Fernando <79061428+C41m@users.noreply.github.com>
2024-01-20 15:32:47 -0300

    Add files via upload

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,8 @@
 import "./hero.scss"
 import {motion} from "framer-motion"
+import { Canvas } from '@react-three/fiber';
+import { MeshDistortMaterial, OrbitControls, Sphere } from '@react-three/drei';
+
 
 const textVarians = {
     initial: {
@@ -38,6 +41,21 @@ const sliderVariants = {
     }   
 }
 
+const imgVariants = {
+    initial: {
+        x: 500, 
+        opacity: 0
+    },
+    animate: {
+        x: 0, 
+        opacity: 1,
+        transition: {
+            duration: 2,
+        }
+    }
+}
+
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -47,15 +65,15 @@ const Hero = () => {
             initial="initial" 
             animate="animate"
             >
-                <motion.h2 variants={textVarians}>CAIO FERNANDO</motion.h2>
+                <motion.h2 variants={textVarians}>BEM VINDO!</motion.h2>
                 <motion.h1 variants={textVarians}>Engenheiro e Analista de Dados</motion.h1>
                 <motion.h1 variants={textVarians}>Python Dev</motion.h1>
                 <motion.div className="buttons" variants={textVarians}>
                     <motion.button variants={textVarians}>
-                        <p>Trabalhos</p>
+                        <a href="#Servicos">Trabalhos</a>
                     </motion.button>
                     <motion.button variants={textVarians}>
-                        <p>Contato</p>
+                        <a href="#Contato">Contato</a>
                     </motion.button>
                 </motion.div>
                 <motion.img variants={textVarians} animate="scrollButton" src="/scroll.png" alt=""/>
@@ -64,9 +82,17 @@ const Hero = () => {
         <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
             Caio Fernando Brito Soares
         </motion.div>
-        <div className="imageContainer">
-            <img src="./hero.png" alt="" />
-        </div>
+        <motion.div className="imageContainer" variants={imgVariants} initial="initial" animate="animate">          
+            <img id="rocketHero" src="./rocketHero.png"/>
+            <Canvas camera={{ fov: 25, position: [5, 5, 5]}}>
+                <OrbitControls enableZoom={false} autoRotate />
+                <ambientLight intensity={0.5}/>
+                <directionalLight position={[3,2,1]}/>
+                <Sphere args={[1, 150, 200]} scale={1.3}>
+                    <MeshDistortMaterial color="orange" attach="material"  distort={0.3} speed={2}/>
+                </Sphere>
+            </Canvas>
+        </motion.div>
     </div>
   )
 }
